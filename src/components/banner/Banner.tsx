48ff2007a5867f16252be7072638e4fdfb6dab0a
mintaek <mintaek970708@gmail.com>
2023-11-23 16:59:19 +0900

    feat: Banner

---
+++ src/components/banner/Banner.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Banner() {
+    return (
+        <SBanner>
+            <BannerTextWrapper>
+                <p>포용금융을 실현하는</p>
+                <p>핀테크 플랫폼</p>
+            </BannerTextWrapper>
+        </SBanner>
+    );
+}
+
+const SBanner = styled.div`
+    width: 100%;
+    height: auto;
+    padding: 128px 0px;
+    margin-left: 167.5px;
+`
+
+const BannerTextWrapper = styled.div`
+    font-size: 100px;
+    font-style: normal;
+    font-weight: bold;
+`
+
