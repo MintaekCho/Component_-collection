7d190029d1db83a2fed0f43a7620827e7dd39b19
mintaek <mintaek970708@gmail.com>
2023-11-23 16:01:23 +0900

    feat: Label

--- src/components/label/Label.tsx
+++ src/components/label/Label.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from "styled-components";
 
-export default function Label() {
+type Props = {
+    text: string;
+    color?: string;
+    fontSize?: string;
+}
+
+export default function Label({
+    text,
+    color='black',
+    fontSize='1rem'
+}: Props) {
     return (
-        <div>
-            
-        </div>
+        <SLabel $color={color} $fontSize={fontSize}>{text}</SLabel>
     );
 }
+
+const SLabel = styled.label<{$color: string, $fontSize: string}>`
+    color: ${({$color}) => $color};
+    font-size: ${({$fontSize}) => $fontSize};
+    font-weight: 600;
+`
 
