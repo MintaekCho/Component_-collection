a622f30b63945ae1f9748b5841b4a1fb562dd3c0
mintaek <mintaek970708@gmail.com>
2023-11-23 16:59:35 +0900

    App 레이아웃 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import InputTest from "./components/input/InputTest";
+import Banner from "./components/banner/Banner";
+import Card from "./components/card/Card";
+import Header from "./components/header/Header";
 
 function App() {
-
   return (
     <>
-      <InputTest />
+      <Header />
+      <Banner />
+      <Card
+        title="Mobild banking"
+        desc="Odio euismod lacinia at quis. Amet purus gravida quis blandit turpis."
+        icon={{path: '/icon/card_icon.svg', bgColor: '#F7DEFF'}}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
